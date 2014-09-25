3ba929df7d0b2a60ec3dbad55211e035ec7d023d
zhenbao.zhou <zhenbao.zhou@qunar.com>
2014-09-25 16:15:20 +0800

    add dozer and jacksonmapper

---
+++ examples/core/main/java/org/gambol/examples/mappers/JacksonMapper.java
@@ -0,0 +1,137 @@
+package org.gambol.examples.mappers;
+
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+import org.codehaus.jackson.map.ser.impl.SimpleFilterProvider;
+import org.codehaus.jackson.type.TypeReference;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * User: zhenbao.zhou
+ * Date: 9/25/14
+ * Time: 4:02 PM
+ */
+public class JacksonMapper {
+    private static Logger logger = LoggerFactory.getLogger(JacksonMapper.class);
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        //config
+        objectMapper.disable(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.setFilters(new SimpleFilterProvider().setFailOnUnknownId(false));
+        objectMapper.setSerializationInclusion(Inclusion.NON_EMPTY);
+    }
+
+    /**
+     * Object => String
+     * @param src
+     * @return
+     */
+    public static <T> String obj2String(T src) {
+        if (src == null) {
+            return null;
+        }
+
+        try {
+            return src instanceof String ? (String) src : objectMapper.writeValueAsString(src);
+        } catch (Exception e) {
+            logger.info("parse Object to String error", e);
+            return null;
+        }
+    }
+
+    /**
+     * Object => byte[]
+     * @param src
+     * @return
+     */
+    public static <T> byte[] obj2Byte(T src) {
+        if (src == null) {
+            return null;
+        }
+
+        try {
+            return src instanceof byte[] ? (byte[]) src : objectMapper.writeValueAsBytes(src);
+        } catch (Exception e) {
+            logger.info("parse Object to byte[] error", e);
+            return null;
+        }
+    }
+
+    /**
+     * String => Object
+     * @param str
+     * @param clazz
+     * @return
+     */
+    public static <T> T string2Obj(String str, Class<T> clazz) {
+        if (str == null || clazz == null) {
+            return null;
+        }
+        try {
+            return clazz.equals(String.class) ? (T)str : objectMapper.readValue(str, clazz);
+        } catch (Exception e) {
+            logger.info("parse String to Object error, String:{}, Class<T>:{}, error:{}", str, clazz.getName(), e);
+            return null;
+        }
+    }
+
+    /**
+     * byte[] => Object
+     * @param bytes
+     * @param clazz
+     * @return
+     */
+    public static <T> T byte2Obj(byte[] bytes, Class<T> clazz) {
+        if (bytes == null || clazz == null) {
+            return null;
+        }
+        try {
+            return clazz.equals(byte[].class) ? (T)bytes : objectMapper.readValue(bytes, clazz);
+        } catch (Exception e) {
+            logger.info("parse byte[] to Object error, byte[]:{}, Class<T>:{}, error:{}", bytes, clazz.getName(), e);
+            return null;
+        }
+    }
+
+    /**
+     * String => Object
+     * @param str
+     * @param typeReference
+     * @return
+     */
+    public static <T> T string2Obj(String str, TypeReference<T> typeReference) {
+        if (str == null || typeReference == null) {
+            return null;
+        }
+        try {
+            return (T) (typeReference.getType().equals(String.class) ? str : objectMapper.readValue(str, typeReference));
+        } catch (Exception e) {
+            logger.info("parse String to Object error, String:{}, TypeReference<T>:{}, error:{}", str, typeReference.getType(), e);
+            return null;
+        }
+    }
+
+    /**
+     * byte[] => Object
+     * @param bytes
+     * @param typeReference
+     * @return
+     */
+    public static <T> T byte2Obj(byte[] bytes, TypeReference<T> typeReference) {
+        if (bytes == null || typeReference == null) {
+            return null;
+        }
+        try {
+            return (T) (typeReference.getType().equals(byte[].class) ? bytes : objectMapper.readValue(bytes, typeReference));
+        } catch (Exception e) {
+            logger.info("parse byte[] to Object error, byte[]:{}, TypeReference<T>:{}, error:{}", bytes, typeReference.getType(), e);
+            return null;
+        }
+    }
+}
---
+++ examples/core/test/java/org/gambol/modules/mapper/JacksonMapperTest.java
@@ -0,0 +1,100 @@
+package org.gambol.modules.mapper;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import junit.framework.TestCase;
+import org.gambol.examples.mappers.JacksonMapper;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * User: zhenbao.zhou
+ * Date: 9/25/14
+ * Time: 4:10 PM
+ */
+public class JacksonMapperTest extends TestCase {
+
+    @Test
+    public void testToJson() throws Exception {
+        // Bean
+        TestBean bean = new TestBean("A");
+        String beanString = JacksonMapper.obj2String(bean);
+        System.out.println("Bean:" + beanString);
+        assertThat(beanString).isEqualTo("{\"name\":\"A\"}");
+
+        // Map
+        Map<String, Object> map = Maps.newLinkedHashMap();
+        map.put("name", "A");
+        map.put("age", 2);
+        String mapString = JacksonMapper.obj2String(map);
+        System.out.println("Map:" + mapString);
+        assertThat(mapString).isEqualTo("{\"name\":\"A\",\"age\":2}");
+
+        // List<String>
+        List<String> stringList = Lists.newArrayList("A", "B", "C");
+        String listString = JacksonMapper.obj2String(stringList);
+        System.out.println("String List:" + listString);
+        assertThat(listString).isEqualTo("[\"A\",\"B\",\"C\"]");
+
+        // List<Bean>
+        List<TestBean> beanList = Lists.newArrayList(new TestBean("A"), new TestBean("B"));
+        String beanListString = JacksonMapper.obj2String(beanList);
+        System.out.println("Bean List:" + beanListString);
+        assertThat(beanListString).isEqualTo("[{\"name\":\"A\"},{\"name\":\"B\"}]");
+
+        // Bean[]
+        TestBean[] beanArray = new TestBean[] { new TestBean("A"), new TestBean("B") };
+        String beanArrayString = JacksonMapper.obj2String(beanArray);
+        System.out.println("Array List:" + beanArrayString);
+        assertThat(beanArrayString).isEqualTo("[{\"name\":\"A\"},{\"name\":\"B\"}]");
+    }
+
+
+    public static class TestBean {
+
+        private String name;
+        private String defaultValue = "";
+        private String nullValue = null;
+
+        public TestBean() {
+        }
+
+        public TestBean(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDefaultValue() {
+            return defaultValue;
+        }
+
+        public void setDefaultValue(String defaultValue) {
+            this.defaultValue = defaultValue;
+        }
+
+        public String getNullValue() {
+            return nullValue;
+        }
+
+        public void setNullValue(String nullValue) {
+            this.nullValue = nullValue;
+        }
+
+        @Override
+        public String toString() {
+            return "TestBean [defaultValue=" + defaultValue + ", name=" + name + ", nullValue=" + nullValue + "]";
+        }
+    }
+
+}
